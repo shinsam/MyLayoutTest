61b3b2d622ca6bc4bd4b78b0ac663d0e0e0a5ad5
shinsam <shin321gg@gmail.com>
2023-06-08 11:23:37 +0900

    first commit

---
+++ app/src/main/java/com/example/mylayouttest/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.mylayouttest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button main_btn1, main_btn2, main_btn3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        main_btn1 = findViewById(R.id.main_btn1);
+        main_btn2 = findViewById(R.id.main_btn2);
+        main_btn3 = findViewById(R.id.main_btn3);
+
+        //다음시간에 할일 : 수행연습 4가지 버튼 이벤트
+    }
+
+
+}
